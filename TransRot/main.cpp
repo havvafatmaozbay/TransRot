9d66c7de11900eac7b009aff5d323a8e25752869
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-06-15 21:23:31 +0300

    Initial Commit

---
+++ TransRot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TransRot
+//
+//  Created by Havva Fatma Özbay on 15.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
